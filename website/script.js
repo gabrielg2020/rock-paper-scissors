56123b887bbd05e7779a59d2de941f2eb6a1b4bd
Gabriel Guimaraes <gabriel.mg04@outlook.com>
2023-09-21 13:14:08 +0100

    Adding user input validation Using a case insentive regex rule to validate input.

---
+++ website/script.js
@@ -0,0 +1,12 @@
+// Take the user input
+let validChoiceSelected = false;
+const regexPattern = /rock|paper|scissors/i;
+
+while (validChoiceSelected === false) {
+    let selection = prompt("Select: Rock, Paper, Scissors!")
+
+    // Validate user choice
+    if (regexPattern.test(selection)) {
+        validChoiceSelected = true
+    }
+}
