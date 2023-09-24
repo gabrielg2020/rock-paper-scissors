7381e2830667ea3e85b356d12c04d4b999bffe50
Gabriel Guimaraes <gabriel.mg04@outlook.com>
2023-09-24 18:24:16 +0100

    HTML and CSS completed I forgot to commit earlier...

--- website/script.js
+++ website/script.js
@@ -1,18 +1,1 @@
-// Take the user input
-let validChoiceSelected = false
-const regexPattern = /rock|paper|scissors/i
-while (validChoiceSelected === false) {
-    let userSelection = prompt("Select: Rock, Paper, Scissors!")
-    // Validate user choice
-    if (regexPattern.test(userSelection)) {
-        validChoiceSelected = true
-        userSelection.toLowerCase()
-    } else {
-        userSelection = undefined
-    }
-}
 
-// Browser choosing which one
-const choices = ['rock', 'paper', 'scissors']
-let browserSelection = choices[Math.floor(Math.random() * choices.length)]
-console.log(browserSelection)
