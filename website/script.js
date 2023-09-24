bd0a4f928a94bf3ac8b90a271cbd4ad31d9bc3e6
Gabriel Guimaraes <gabriel.mg04@outlook.com>
2023-09-24 19:26:07 +0100

    Completed Game I forgot to commit, then i remembered, then i kept going...

--- website/script.js
+++ website/script.js
@@ -1,1 +1,39 @@
+const cpuResult = document.querySelector('.cpu-result');
+const userResult = document.querySelector('.user-result');
 
+const winsCounter = document.querySelector('.wins');
+const lossesCounter = document.querySelector('.losses');
+const drawsCounter = document.querySelector('.draws');
+const status = document.querySelector('.status');
+
+// Add event listeners to the buttons
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        playGame(button.textContent);
+    });
+});
+
+function playGame(type) {
+    userResult.textContent = type;
+    cpuResult.textContent = cpuSelect();
+    evaluateGame(type, cpuResult.textContent)
+};
+
+function cpuSelect() {
+    const choices = ['🪨','📜','✂️'];
+    return choices[Math.floor(Math.random() * choices.length)];
+};
+
+function evaluateGame(user, cpu) {
+    if (user === cpu) {
+        drawsCounter.textContent = Number(drawsCounter.textContent) + 1
+        status.textContent = 'Draw!'
+    } else if (user === '🪨' && cpu === '✂️' || user === '📜' && cpu === '🪨' || user === '✂️' && cpu === '📜') {
+        winsCounter.textContent = Number(winsCounter.textContent) + 1
+        status.textContent = 'You won!'
+    } else {
+        lossesCounter.textContent = Number(lossesCounter.textContent) + 1
+        status.textContent = 'You lost!'
+    }
+};
